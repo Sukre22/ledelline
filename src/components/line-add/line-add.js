a5621287cea23dc018edb96c14b0cda727818fcb
Исаев Раиль Рамилевич <r.isaev@ledel.local>
2022-01-17 17:00:47 +0300

    edited section 2

--- src/components/line-add/line-add.js
+++ src/components/line-add/line-add.js
@@ -3,16 +3,17 @@ import './line-add.css';
 
 const LineAdd = () => {
     return (
-     <div className={'input-container'}>        
+     <div className={'input-container'}> 
+      <div className={'input-container_description'}>
+            <p> Внимание! Длина непрерывной линии не должна превышать 12 м и должна быть кратна 0,25 м.</p>
+        </div>       
             <div className={'input-container_get'}>
                 <h2 className={'input-container_get-h2'}>Введите длину линии (м):</h2>
                 <input className={'input-container_get-area'} type="text" placeholder='10.25'/>
             </div>
-        <div className={'input-container_description'}>
-            <p> Внимание! Длина непрерывной линии не должна превышать 12 м и должна быть кратна 0,25 м.</p>
-        </div>
+       
             <div className={'input-container_buttons'}>
-                <button>Добавить новую линию</button>
+                <button className={'input-container_buttons-first'}>Добавить новую линию</button>
                 <button>Вывести общую спецификацию</button>
             </div>        
      </div>
--- src/components/line-list-item/line-list-item.js
+++ src/components/line-list-item/line-list-item.js
@@ -10,7 +10,14 @@ const LineListItem = () => {
                  <div className={'line-list-item_container-wrap-results'}>
                         <h2 className={'line-list-item_container-wrap-results-head'}>Полученные результаты:</h2>
                         <div className={'line-list-item_container-wrap-results-wrapper'}>
-                              111111111111111111111111111111111111111111
+                            <h2 className={'line-list-item_container-wrap-results-wrapper-h2'}>Светодиодные модули:</h2>
+                        <ul className={'line-list-item_container-wrap-results-wrapper-modules'}>
+                            <li>L-line A 0,25 - 1 шт.</li>
+                            <li>L-line A 0,5 - 1шт.</li>
+                            <li>L-line A 1 - 1 шт.</li>
+                            <li>L-line A 1,5 - 1 шт.</li>
+                            <li>L-line A 3 - 1 шт.</li>
+                        </ul>
                         </div>
                  </div>
 

705d65c7b3dd05c138cdcb3f30e794246db76b95
Исаев Раиль Рамилевич <r.isaev@ledel.local>
2022-01-17 14:33:16 +0300

    worked at section 3

--- src/components/line-list-item/line-list-item.js
+++ src/components/line-list-item/line-list-item.js
@@ -5,6 +5,17 @@ const LineListItem = () => {
     return (
         <div className={'line-list-item_container'}>
              <h2 className={'line-list-item_container-description'}>Линия №1</h2>
+             <div clasccName={'line-list-item_container-wrap'}>
+
+                 <div className={'line-list-item_container-wrap-results'}>
+                        <h2 className={'line-list-item_container-wrap-results-head'}>Полученные результаты:</h2>
+                        <div className={'line-list-item_container-wrap-results-wrapper'}>
+                              111111111111111111111111111111111111111111
+                        </div>
+                 </div>
+
+             </div>
+             <button className={'line-list-item_container-button'}>Удалить линию</button>
         </div>
        
     )

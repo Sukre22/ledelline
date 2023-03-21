7aca8895da3e7f392b3a7a1a8ed2b9a93ecae72f
Исаев Раиль Рамилевич <r.isaev@ledel.local>
2023-03-21 11:50:56 +0300

    router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom';
 import './index.css';
 import App from './components/app/app.js';
 
 
-ReactDOM.render(
+/*ReactDOM.render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
